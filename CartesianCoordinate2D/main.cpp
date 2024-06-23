2db8d013343821553eda0279f501269e78b7b22f
Aniket Sedhai <asedhai@masonite.com>
2024-06-22 23:24:16 -0500

    Initial Commit

---
+++ CartesianCoordinate2D/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CartesianCoordinate2D
+//
+//  Created by Aniket Sedhai on 6/22/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
